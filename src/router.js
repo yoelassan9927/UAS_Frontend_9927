f0d80e7e85efbdca6f348e7bda1fc73bd696f8a1
User <yoelcassan01@gmail.com>
2020-11-13 00:35:19 +0800

    GD Vue CLI-Webpack

---
+++ src/router.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+function importComponent(path) {
+ return () => import(`./components/${path}.vue`)
+}
+Vue.use(VueRouter);
+const router = new VueRouter({
+ mode: "history",
+ routes: [
+ {
+ path: "/",
+ name: "admin",
+ component: importComponent('DashboardLayout'),
+ children: [
+ //Dashboard
+ {
+ path: "/",
+ name: "Root",
+ component: importComponent('Dashboard'),
+ },
+ // To do list
+ {
+ path: "/gd",
+ name: "Guided",
+ component: importComponent('TodoList/List'),
+ },
+ ]
+ },
+ ]
+});
+export default router;
